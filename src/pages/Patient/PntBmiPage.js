53d4eb78b1d771beed27d70e96009a33135ff9f5
MeriSg <sgmerisg@gmail.com>
2020-09-04 12:57:41 +0300

    Real time update weight

--- src/pages/Patient/PntBmiPage.js
+++ src/pages/Patient/PntBmiPage.js
@@ -3,7 +3,9 @@ import PntNavBar from '../../components/PntNavBar';
 import { Redirect } from 'react-router-dom';
 import BmiView from '../../components/BmiView';
 import { Form , Col, Row, Button } from 'react-bootstrap';
-import './PntBmiPage.css'
+import './PntBmiPage.css';
+import Parse from 'parse';
+import moment from 'moment';
 
 class PntBmiPage extends Component {
 
@@ -16,7 +18,39 @@ class PntBmiPage extends Component {
     }
 
     updateWeight = () =>{
+        const {weightInput} = this.state;
 
+        const User = new Parse.User();
+        const query = new Parse.Query(User);
+        
+        // Finds the user by its ID
+        query.get(Parse.User.current().id).then((user) => {
+          // Updates the data we want      
+          user.set('weight', Number( weightInput));        
+          // Saves the user with the updated data
+          user.save().then((response) => {
+            console.log('Updated user', response);
+          }).catch((error) => {
+            console.error('Error while updating user', error);
+          });
+        });
+
+        const WeightTracking = Parse.Object.extend('WeightTracking');
+        const myNewObject = new WeightTracking();
+        
+        myNewObject.set('UserId', Parse.User.current());
+        myNewObject.set('pntId', Parse.User.current().id+"");
+        myNewObject.set('date',  moment().format("DD-MM-YYYY"));
+        myNewObject.set('weight', weightInput);
+        
+        myNewObject.save().then(
+            (result) => {
+            console.log('WeightTracking created', result);
+            },
+            (error) => {
+            console.error('Error while creating WeightTracking: ', error);
+            }
+        );
     }
 
     render() {
@@ -44,7 +78,7 @@ class PntBmiPage extends Component {
                         <Button variant="primary" size="lg" onClick={this.updateWeight} block variant="success">Update Weight </Button>
                     </Form.Group>
                </Form>
-               <BmiView className="bmi-view" userName={activeUser.fname+" "+activeUser.lname}  pntHeight={activeUser.height} pntWeight={activeUser.weight} pntIsMale={activeUser.ismale}/>
+               <BmiView className="bmi-view" userName={activeUser.fname+" "+activeUser.lname}  pntHeight={activeUser.height} pntWeight={weightInput} pntIsMale={activeUser.ismale}/>
 
             </div>
         );
