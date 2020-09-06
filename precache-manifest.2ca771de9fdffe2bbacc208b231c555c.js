d7892e5a7314681e2af82802bd2cb6e2914e81b9
MeriSg <sgmerisg@gmail.com>
2020-09-06 23:13:12 +0300

    Updates

---
+++ precache-manifest.2ca771de9fdffe2bbacc208b231c555c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74011fee4a45fdea9b7d57d24489a6a",
+    "url": "/end-project-react/index.html"
+  },
+  {
+    "revision": "081b5c040313aa600749",
+    "url": "/end-project-react/static/css/2.cfbe174d.chunk.css"
+  },
+  {
+    "revision": "f1de048ffb540f3b7282",
+    "url": "/end-project-react/static/css/main.4c4fb7b8.chunk.css"
+  },
+  {
+    "revision": "081b5c040313aa600749",
+    "url": "/end-project-react/static/js/2.c323297e.chunk.js"
+  },
+  {
+    "revision": "783268c21bfb235a0ecad752a8fc462f",
+    "url": "/end-project-react/static/js/2.c323297e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1de048ffb540f3b7282",
+    "url": "/end-project-react/static/js/main.df7ddbba.chunk.js"
+  },
+  {
+    "revision": "eae6afe78d0eab165ce6",
+    "url": "/end-project-react/static/js/runtime-main.8045ecac.js"
+  }
+]);
