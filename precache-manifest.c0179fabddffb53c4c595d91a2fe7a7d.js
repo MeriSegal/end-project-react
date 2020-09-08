a7a6c403f2adf00bc625042ce1045455085f7ae9
MeriSg <sgmerisg@gmail.com>
2020-09-08 20:11:41 +0300

    Updates

--- precache-manifest.c0179fabddffb53c4c595d91a2fe7a7d.js
+++ precache-manifest.c0179fabddffb53c4c595d91a2fe7a7d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3138045e374ca01e870598d4ee19f6b3",
+    "revision": "de27476b1555cecf2a179b15646a2189",
     "url": "/end-project-react/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/end-project-react/static/css/2.cfbe174d.chunk.css"
   },
   {
-    "revision": "e74b30c4377c4f9c731d",
+    "revision": "d3e89ff868fbf79936fb",
     "url": "/end-project-react/static/css/main.5485fb45.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/end-project-react/static/js/2.c323297e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e74b30c4377c4f9c731d",
-    "url": "/end-project-react/static/js/main.3334bc31.chunk.js"
+    "revision": "d3e89ff868fbf79936fb",
+    "url": "/end-project-react/static/js/main.f5eec5ef.chunk.js"
   },
   {
     "revision": "eae6afe78d0eab165ce6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/end-project-react/precache-manifest.c75d38275b965f2ce0efdf80bf7718ad.js"
+  "/end-project-react/precache-manifest.c0179fabddffb53c4c595d91a2fe7a7d.js"
 );
 
 self.addEventListener('message', (event) => {
